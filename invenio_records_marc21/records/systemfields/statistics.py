3ed309854455c2a3b513a6c954e62af54c90cdbe
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-09-28 21:13:58 +0200

    fix: utcnow + refactor

--- invenio_records_marc21/records/systemfields/statistics.py
+++ invenio_records_marc21/records/systemfields/statistics.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2023 TU Wien.
-# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024-2025 Graz University of Technology.
 #
 # invenio-records-marc21 is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
