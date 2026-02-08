dcdadfa0faca1d45c0fb3e24a10f412fc90c7351
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-02-08 21:17:53 +0100

    release: v0.28.0

--- invenio_records_marc21/__init__.py
+++ invenio_records_marc21/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 #
-# Copyright (C) 2021-2025 Graz University of Technology.
+# Copyright (C) 2021-2026 Graz University of Technology.
 #
 # Invenio-Records-Marc21 is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -24,7 +24,7 @@ from .services import (
     create_record,
 )
 
-__version__ = "0.27.1"
+__version__ = "0.28.0"
 
 __all__ = (
     "DuplicateRecordError",
