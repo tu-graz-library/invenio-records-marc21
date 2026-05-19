8aeca4d68fbaa4724fe1406cccd9781c4e634238
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-05-19 10:37:25 +0200

    chore(types): improve type hints usage

--- invenio_records_marc21/resources/serializers/datacite/schema.py
+++ invenio_records_marc21/resources/serializers/datacite/schema.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2021 CERN.
 # Copyright (C) 2021 Northwestern University.
-# Copyright (C) 2022-2025 Graz University of Technology.
+# Copyright (C) 2022-2026 Graz University of Technology.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -11,10 +11,15 @@
 """DataCite based Schema for Invenio RDM Records."""
 
 from flask import current_app
-from marshmallow import Schema, fields, missing
+from marshmallow import Schema, missing
+from marshmallow.fields import Constant, Method, Nested
 
 
-def get_scheme_datacite(scheme, config_name, default=None):
+def get_scheme_datacite(
+    scheme: dict,
+    config_name: str,
+    default: str | None = None,
+) -> str:
     """Returns the datacite equivalent of a scheme."""
     config_item = current_app.config[config_name]
     return config_item.get(scheme, {}).get("datacite", default)
@@ -23,9 +28,9 @@ def get_scheme_datacite(scheme, config_name, default=None):
 class CreatorSchema43(Schema):
     """Creator schema for v4."""
 
-    name = fields.Method("get_name")
+    name = Method("get_name")
 
-    def get_name(self, obj):
+    def get_name(self, obj: dict) -> str:
         """Get titles list."""
         names = obj.get("100", [{"subfields": {}}])[0]
         return names.get("subfields", {}).get("a", [""])[0]
@@ -35,15 +40,15 @@ class Marc21DataCite43Schema(Schema):
     """DataCite JSON 4.3 Marshmallow Schema."""
 
     # PIDS-FIXME: What about versioning links and related ids
-    identifiers = fields.Method("get_identifiers")
-    types = fields.Method("get_type")
-    titles = fields.Method("get_titles")
-    creators = fields.Nested(CreatorSchema43, attribute="metadata.fields")
-    publisher = fields.Method("get_publisher")
-    publicationYear = fields.Method("get_publication_year")
-    schemaVersion = fields.Constant("http://datacite.org/schema/kernel-4")
+    identifiers = Method("get_identifiers")
+    types = Method("get_type")
+    titles = Method("get_titles")
+    creators = Nested(CreatorSchema43, attribute="metadata.fields")
+    publisher = Method("get_publisher")
+    publicationYear = Method("get_publication_year")  # noqa: N815
+    schemaVersion = Constant("http://datacite.org/schema/kernel-4")  # noqa: N815
 
-    def get_type(self, obj):
+    def get_type(self, _: dict) -> dict[str, str]:
         """Get resource type."""
         # FIXME: The metadatafield 970 from dojson module needed
         return {
@@ -51,42 +56,39 @@ class Marc21DataCite43Schema(Schema):
             "resourceType": "Text",
         }
 
-    def _get_field(self, obj, field_number, default=""):
+    def _get_field(self, obj: dict, field_number: str, default: None = None) -> dict:
         """Get field from metadata."""
         fields = obj["metadata"].get("fields", {})
         return fields.get(field_number, default)
 
-    def _get_subfields(self, obj, field_number):
+    def _get_subfields(self, obj: dict, field_number: str) -> dict:
         """Get subfields from metadata."""
         return self._get_field(obj, field_number, default=[{"subfields": {}}])[0].get(
             "subfields", {}
         )
 
-    def get_titles(self, obj):
+    def get_titles(self, obj: dict) -> list[dict[str, str]]:
         """Get titles list."""
         titles_field = self._get_subfields(obj, "245")
 
-        titles = []
         title_fields = titles_field.get("a", [""])
-        for title in title_fields:
-            titles.append({"title": title})
-        return titles
+        return [{"title": title} for title in title_fields]
 
-    def get_publisher(self, obj):
+    def get_publisher(self, obj: dict) -> str:
         """Get publisher."""
         for field_number in ["260", "264"]:
             publisher_field = self._get_subfields(obj, field_number)
-            publisher = publisher_field.get("b")
+            publisher: list[str] = publisher_field.get("b")
             if publisher:
                 return publisher[0]
 
         return current_app.config.get("MARC21_DATACITE_DEFAULT_PUBLISHER")
 
-    def get_publication_year(self, obj):
+    def get_publication_year(self, obj: dict) -> str | None:
         """Get publication year from edtf date."""
         publication_dates = self._get_field(obj, "008", None)
         if publication_dates:
-            publication_date = publication_dates[7:11]
+            publication_date: str = publication_dates[7:11]
             return publication_date
 
         # Production, Publication, Distribution, Manufacture, and Copyright Notice
@@ -96,7 +98,7 @@ class Marc21DataCite43Schema(Schema):
 
         return None
 
-    def get_identifiers(self, obj):
+    def get_identifiers(self, obj: dict) -> list | missing:
         """Get (main and alternate) identifiers list."""
         serialized_identifiers = []
 
@@ -114,7 +116,7 @@ class Marc21DataCite43Schema(Schema):
                     {
                         "identifier": id_["identifier"],
                         "identifierType": id_scheme,
-                    }
+                    },
                 )
 
         return serialized_identifiers or missing
--- invenio_records_marc21/services/record/utils.py
+++ invenio_records_marc21/services/record/utils.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 #
-# Copyright (C) 2022-2023 Graz University of Technology.
+# Copyright (C) 2022-2026 Graz University of Technology.
 #
 # Invenio-Records-Marc21 is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -12,34 +12,49 @@
 
 import time
 from functools import singledispatch
-from os.path import basename
+from pathlib import Path
 
+from flask_principal import Identity
+from invenio_records_resources.services import FileService
 from invenio_search import RecordsSearch
 from invenio_search.engine import dsl
 from marshmallow.exceptions import ValidationError
 
+from ...records.api import Marc21Draft
+
+# from ..services import Marc21RecordService
 from .types import DOI, ACNumber, DuplicateRecordError
 
 
 def add_file_to_record(
-    marcid,
-    file_path,
-    file_service,
-    identity,
-):
+    marcid: str,
+    file_path: Path,
+    file_service: FileService,
+    identity: Identity,
+) -> None:
     """Add the file to the record."""
-    filename = basename(file_path)
+    filename = file_path.name
     data = [{"key": filename}]
 
-    with open(file_path, mode="rb") as file_pointer:
+    with file_path.open(mode="rb") as file_pointer:
         file_service.init_files(id_=marcid, identity=identity, data=data)
         file_service.set_file_content(
-            id_=marcid, file_key=filename, identity=identity, stream=file_pointer
+            id_=marcid,
+            file_key=filename,
+            identity=identity,
+            stream=file_pointer,
         )
         file_service.commit_file(id_=marcid, file_key=filename, identity=identity)
 
 
-def create_record(service, data, file_paths, identity, do_publish=True):
+def create_record(
+    service,  # : Marc21RecordService,
+    data: dict,
+    file_paths: list,
+    identity: Identity,
+    *,
+    do_publish: bool = True,
+) -> Marc21Draft:
     """Create the record."""
     draft = service.create(data=data, identity=identity, files=True)
 
@@ -61,13 +76,13 @@ def create_record(service, data, file_paths, identity, do_publish=True):
 
     except (FileNotFoundError, ValidationError) as error:
         service.delete_draft(id_=draft.id, identity=identity)
-        raise error
+        raise error from error
 
     return draft
 
 
 @singledispatch
-def check_about_duplicate(value: str, category: str = None):
+def check_about_duplicate(value: str, category: str | None = None) -> None:
     """Check if the record with the ac number is already within the database."""
     search = RecordsSearch(index="marc21records")
 
@@ -84,11 +99,11 @@ def check_about_duplicate(value: str, category: str = None):
 
 
 @check_about_duplicate.register
-def _(value: ACNumber):
+def _(value: ACNumber) -> None:  # type: ignore[misc]
     """Check about double ac number."""
     check_about_duplicate(str(value), value.category)
 
 
 @check_about_duplicate.register
-def _(value: DOI):
+def _(value: DOI) -> None:  # type: ignore[misc]
     check_about_duplicate(str(value), value.category)
