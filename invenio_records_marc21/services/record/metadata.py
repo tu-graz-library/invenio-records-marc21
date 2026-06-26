51884e8a4f76c7c7f6c6e8b52210f18052717971
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-06-26 12:32:04 +0200

    refactor(typing): Marc21Metadata
    * introduce typing into Marc21Metadata which needed a little bit more
      work as thought.

--- invenio_records_marc21/services/record/metadata.py
+++ invenio_records_marc21/services/record/metadata.py
@@ -10,7 +10,11 @@
 
 """Marc21 record class."""
 
+from collections.abc import Iterator
 from contextlib import suppress
+from dataclasses import dataclass
+from typing import cast
+from warnings import warn
 from xml.etree.ElementTree import Element
 
 
@@ -105,7 +109,7 @@ def convert_marc21xml_to_json(record):
 class XmlToJsonVisitor:
     """XmlToJsonVisitor class."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Constructor."""
         self.record = {"leader": "", "fields": {}}
 
@@ -182,30 +186,210 @@ class XmlToJsonVisitor:
         self.subfields[subf_code].append(node.text)
 
 
+@dataclass
+class Field:
+    """Base field."""
+
+    category: str
+
+    def __init__(self, category: str | None = None) -> None:
+        """Construct."""
+        self._list: list[Field] = []
+
+        if category:
+            self.category = category
+
+    def append(self, field: "Field") -> None:
+        """Append."""
+        self._list.append(field)
+
+    def dump(self) -> list:
+        """Dump."""
+        return [field.to_obj() for field in self._list]
+
+    def to_obj(self) -> dict:
+        """To obj."""
+        return {}
+
+    def __iter__(self) -> Iterator["Field"]:
+        """Iter."""
+        return iter(self._list)
+
+
+@dataclass
+class ControlField(Field):
+    """Control field."""
+
+    value: str
+
+    def __str__(self) -> str:
+        """Stringify."""
+        return self.value
+
+    def dump(self) -> str:  # type: ignore[override]
+        """Dump."""
+        return self.value
+
+
+@dataclass
+class DataField(Field):
+    """Data field."""
+
+    ind1: str
+    ind2: str
+    subfields: dict[str, list[str]]
+
+    def __init__(
+        self,
+        selector: str,
+        *,
+        value: str | None = None,
+        subfs: dict | None = None,
+    ) -> None:
+        """Construct datafield."""
+        super().__init__()
+
+        tag, ind1, ind2, code = selector.split(".")
+
+        if not ind1:
+            ind1 = "_"
+
+        if not ind2:
+            ind2 = "_"
+
+        if not code:
+            code = "a"
+
+        self.category = tag
+        self.ind1 = ind1
+        self.ind2 = ind2
+        self.subfields = {}
+
+        if value:
+            self.subfields[code] = [value]
+        elif subfs:
+            for key, val in sorted(
+                subfs.items(),
+                key=lambda x: f"zz{x}" if x[0].isnumeric() else x[0],
+            ):
+                self.subfields[key] = val if isinstance(val, list) else [val]
+        else:
+            msg = "Neither of value or subfs is set."
+            raise RuntimeError(msg)
+
+        self._list.append(self)
+
+    def get(self, subf_code: str) -> str:
+        """Get."""
+        return " ".join(self.subfields.get(subf_code, []))
+
+    def to_obj(self) -> dict:
+        """To obj."""
+        return {
+            "ind1": self.ind1,
+            "ind2": self.ind2,
+            "subfields": self.subfields,
+        }
+
+    def __getitem__(self, key: str | int) -> DataField | dict:
+        """Getitem."""
+        warn(
+            "don't use subscription on DataField. please correct the code",
+            DeprecationWarning,
+            2,
+        )
+        if isinstance(key, int):
+            return self
+
+        if isinstance(key, str) and key == "subfields":
+            return self.subfields
+
+        raise RuntimeError
+
+
+@dataclass
+class Marc21MetadataJson:
+    """Dataclass for Marc21Metatada json structure.
+
+    leader: "00000nam a2200000zca4500",
+    fields: {
+        100: [
+          {
+             ind1: "",
+             ind2: "",
+             subfields:
+          }
+        ]
+    }
+    """
+
+    leader: str
+    fields: dict[str, Field]
+
+    def add(self, field: Field) -> None:
+        """Add."""
+        self.fields[field.category] = field
+
+    def extend(self, field: Field) -> None:
+        """Extend."""
+        self.fields[field.category].append(field)
+
+    def emplace(self, field: DataField) -> None:
+        """Emplace."""
+        # NOTE: only implemented for DataField, because it does not make sense for ControlField
+
+        # if the tag already exists it has to be found the correct ind1/ind2
+        # combination to update subfields. dict does not deep update as
+        # intended
+        datafields = self.fields[field.category]
+        for d in cast(list[DataField], datafields):
+            if d.ind1 == field.ind1 and d.ind2 == field.ind2:
+                for code, value in field.subfields.items():
+                    if code in d.subfields:
+                        d.subfields[code].extend(value)
+                    else:
+                        d.subfields[code] = value
+
+    def dump(self) -> dict:
+        """Dump."""
+        return {
+            "leader": self.leader,
+            "fields": {tag: field.dump() for tag, field in self.fields.items()},
+        }
+
+
 class Marc21Metadata:
     """MARC21 Record class to facilitate storage of records in MARC21 format."""
 
-    def __init__(self, *, json=None):
+    metadata: Marc21MetadataJson
+
+    DATAFIELD_START_CATEGORY = 10
+
+    def __init__(self, *, json: dict | None = None) -> None:
         """Default constructor of the class."""
         self.set_default()
 
         if json:
             self.json = json
 
-    def set_default(self):
+    def set_default(self) -> None:
         """Set default marc21 structure."""
-        self._json = {
-            "leader": "00000nam a2200000zca4500",
-            "fields": {},
-        }
+        self.metadata = Marc21MetadataJson("00000nam a2200000zca4500", {})
+
+    def is_control_field(self, category: str) -> bool:
+        """Is control field."""
+        try:
+            return int(category) < self.DATAFIELD_START_CATEGORY
+        except ValueError:
+            return False
 
     @property
-    def json(self):
+    def json(self) -> dict:
         """Metadata json getter method."""
-        return {"metadata": self._json}
+        return {"metadata": self.metadata.dump()}
 
     @json.setter
-    def json(self, _json):
+    def json(self, _json: dict) -> None:
         """Metadata json setter method."""
         if not (
             isinstance(_json, dict)
@@ -218,9 +402,26 @@ class Marc21Metadata:
             )
             raise TypeError(msg)
 
-        self._json = _json["metadata"] if "metadata" in _json else _json
+        _json = _json.get("metadata", _json)
 
-    def get_field(self, selector: str, *, subf_value: str = "") -> dict:
+        self.metadata.leader = _json["leader"]
+
+        for tag, field in _json["fields"].items():
+            try:
+                int(tag)  # filter out AV* fields
+            except ValueError:
+                continue
+
+            if self.is_control_field(tag):
+                self.metadata.add(ControlField(tag, field))
+            else:
+                datafields = Field(tag)
+                for fie in field:
+                    selector = f"{tag}.{fie['ind1']}.{fie['ind2']}."
+                    datafields.append(DataField(selector, subfs=fie["subfields"]))
+                self.metadata.add(datafields)
+
+    def get_field(self, selector: str, *, subf_value: str = "") -> DataField | None:
         """Get Field."""
         category, ind1, ind2, subf_code = selector.split(".")
         _, datafields = self.get_fields(category, ind1, ind2)
@@ -229,78 +430,85 @@ class Marc21Metadata:
             if (
                 subf_code
                 and subf_value
-                and field["subfields"].get(subf_code, [None])[0] == subf_value
+                and field.subfields.get(subf_code, [None])[0] == subf_value
             ):
                 out.append(field)
-            elif subf_code and not subf_value and subf_code in field["subfields"]:
+            elif subf_code and not subf_value and subf_code in field.subfields:
                 out.append(field)
             elif (
                 not subf_code
                 and subf_value
-                and [subf_value] in field["subfields"].values()
+                and [subf_value] in field.subfields.values()
             ):
                 out.append(field)
             elif not subf_code and not subf_value:
                 out.append(field)
 
-        return out[0]
+        return out[0] if len(out) > 0 else None
 
     def get_fields(
         self,
         category: str,
-        ind1: str = None,
-        ind2: str = None,
-        return_type: str = "xml",
-    ) -> tuple[list[Element], list[Element]]:
+        ind1: str | None = None,
+        ind2: str | None = None,
+    ) -> tuple[ControlField | None, list[DataField]]:
         """Return fields found by category.
 
         The return value could be found more precisely by defining ind1, ind2
         and subf_code.
         """
         if not category.isnumeric():
-            return ([], [])
+            return None, []
+
+        controlfield: ControlField | None = None
+        datafields: list[DataField] = []
 
         try:
-            if int(category) < 10:
-                controlfields = [self._json["fields"][category]]
-            else:
-                controlfields = []
+            if self.is_control_field(category):
+                controlfield = cast(ControlField, self.metadata.fields[category])
+                return controlfield, datafields
         except KeyError:
-            controlfields = []
+            controlfield = None
 
-        def fix_underline(ind):
+        def fix_underline(ind: str) -> list[str | None]:
             # the problem is that the codebase introduced "_" as a replacement
             # for " " and this has to be fixed somewhere in the future
             return [ind, " " if ind == "_" else None, "_" if ind == " " else None]
 
-        def ind_condition(d):
-            ind1_ = d["ind1"] in fix_underline(ind1) if ind1 else True
-            ind2_ = d["ind2"] in fix_underline(ind2) if ind2 else True
+        def ind_condition(d: DataField) -> bool:
+            ind1_ = d.ind1 in fix_underline(ind1) if ind1 else True
+            ind2_ = d.ind2 in fix_underline(ind2) if ind2 else True
             return ind1_ and ind2_
 
         try:
-            datafields = [d for d in self._json["fields"][category] if ind_condition(d)]
+            datafields = [
+                d
+                for d in cast(list[DataField], self.metadata.fields[category])
+                if ind_condition(d)
+            ]
         except KeyError:
             datafields = []
+        else:
+            return controlfield, datafields
 
-        return (controlfields, datafields)
+        return controlfield, datafields
 
     def get_value(
         self,
         category: str,
-        ind1: str = None,
-        ind2: str = None,
-        subf_code: str = None,
+        ind1: str | None = None,
+        ind2: str | None = None,
+        subf_code: str | None = None,
     ) -> str:
         """Get the value of the found field."""
-        controlfields, datafields = self.get_fields(category, ind1, ind2)
+        controlfield, datafields = self.get_fields(category, ind1, ind2)
 
-        if len(controlfields) > 0:
-            return controlfields[0]
+        if controlfield:
+            return str(controlfield)
 
         if len(datafields) > 0:
             try:
-                return " ".join(datafields[0]["subfields"][subf_code])
+                return " ".join(datafields[0].subfields[subf_code or ""])
             except KeyError:
                 return ""
 
@@ -309,53 +517,59 @@ class Marc21Metadata:
     def get_values(
         self,
         category: str,
-        ind1: str = None,
-        ind2: str = None,
-        subf_code: str = None,
+        ind1: str | None = None,
+        ind2: str | None = None,
+        subf_code: str | None = None,
     ) -> list[str]:
         """Get values of the found field."""
-        controlfields, datafields = self.get_fields(category, ind1, ind2)
+        controlfield, datafields = self.get_fields(category, ind1, ind2)
 
-        values = []
+        values: list[str] = []
 
-        for controlfield in controlfields:
-            values.append(controlfield)
+        if controlfield:
+            values.append(str(controlfield))
 
         for datafield in datafields:
             if subf_code is None:
-                for value in datafield["subfields"].values():
-                    values.extend(value)
+                values.extend(" ".join(d) for d in datafield.subfields.values())
 
             with suppress(KeyError):
-                values.extend(datafield["subfields"][subf_code])
+                values.extend(datafield.subfields[subf_code or ""])
 
         return values
 
     def exists_field(
         self,
         category: str,
-        ind1: str = None,
-        ind2: str = None,
-        subf_code: str = None,
-        subf_value: str = None,
+        ind1: str | None = None,
+        ind2: str | None = None,
+        subf_code: str | None = None,
+        subf_value: str | None = None,
     ) -> bool:
         """Check if the field exists."""
         values = self.get_values(category, ind1, ind2, subf_code)
-        return any((value == subf_value for value in values))
+        return any(value == subf_value for value in values)
 
-    def emplace_leader(self, value=""):
+    def emplace_leader(self, value: str = "") -> None:
         """Change leader string in record."""
-        self._json["leader"] = value
+        self.metadata.leader = value
 
-    def emplace_controlfield(self, tag="", value=""):
+    def emplace_controlfield(self, tag: str = "", value: str = "") -> None:
         """Add value to record for given datafield and subfield."""
-        if int(tag) > 9:
-            raise RuntimeError("ERROR: controlfields are < 10")
+        if not self.is_control_field(tag):
+            msg = "ERROR: controlfields are < 10"
+            raise RuntimeError(msg)
 
-        controlfield = {tag: value}
-        self._json["fields"].update(controlfield)
+        controlfield = ControlField(tag, value)
+        self.metadata.add(controlfield)
 
-    def emplace_datafield(self, selector, *, value=None, subfs=None) -> None:
+    def emplace_datafield(
+        self,
+        selector: str,
+        *,
+        value: str | None = None,
+        subfs: dict | None = None,
+    ) -> None:
         """Emplace value to record for given datafield and subfield.
 
         :params selector e.g. "100...a", "100"
@@ -369,72 +583,28 @@ class Marc21Metadata:
         On the first addition emplace_datafield and add_datafield work the same,
         the difference only becomes apparent when an identical field is added.
         """
-        tag, ind1, ind2, code = selector.split(".")
-        datafield = self._datafield(selector, value=value, subfs=subfs)
+        datafield = DataField(selector, value=value, subfs=subfs)
 
-        if tag not in self._json["fields"]:
-            self._json["fields"].update(datafield)
+        if datafield.category not in self.metadata.fields:
+            self.metadata.add(datafield)
         else:
-            # if the tag already exists it has to be found the correct ind1/ind2
-            # combination to update subfields. dict does not deep update as
-            # intended
-            datafields = self._json["fields"][tag]
-            for d in datafields:
-                if d["ind1"] == ind1 and d["ind2"] == ind2:
-                    for code, value in datafield[tag][0]["subfields"].items():
-                        if code in d["subfields"]:
-                            d["subfields"][code].extend(value)
-                        else:
-                            d["subfields"][code] = value
+            self.metadata.emplace(datafield)
 
-    def add_datafield(self, selector, *, value=None, subfs=None) -> None:
+    def add_datafield(
+        self,
+        selector: str,
+        *,
+        value: str | None = None,
+        subfs: dict | None = None,
+    ) -> None:
         """Add value to record for given datafield and subfield.
 
         This method can be used if two independent fields with the same
         indicator should be added to the fields list.
         """
-        tag, _, _, _ = selector.split(".")
-        datafield = self._datafield(selector, value=value, subfs=subfs)
+        datafield = DataField(selector, value=value, subfs=subfs)
 
-        if tag not in self._json["fields"]:
-            self._json["fields"].update(datafield)
+        if datafield.category not in self.metadata.fields:
+            self.metadata.add(datafield)
         else:
-            self._json["fields"][tag].extend(datafield[tag])
-
-    def _datafield(self, selector, *, value=None, subfs=None) -> dict:
-        """Construct datafield."""
-        tag, ind1, ind2, code = selector.split(".")
-
-        if not ind1:
-            ind1 = "_"
-
-        if not ind2:
-            ind2 = "_"
-
-        if not code:
-            code = "a"
-
-        datafield = {
-            tag: [
-                {
-                    "ind1": ind1,
-                    "ind2": ind2,
-                    "subfields": {},
-                }
-            ],
-        }
-
-        if value:
-            datafield[tag][0]["subfields"][code] = [value]
-        elif subfs:
-            for key, val in sorted(
-                subfs.items(),
-                key=lambda x: f"zz{x}" if x[0].isnumeric() else x[0],
-            ):
-                datafield[tag][0]["subfields"][key] = (
-                    val if isinstance(val, list) else [val]
-                )
-        else:
-            raise RuntimeError("Neither of value or subfs is set.")
-
-        return datafield
+            self.metadata.extend(datafield)
